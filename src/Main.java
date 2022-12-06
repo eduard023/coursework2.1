fa3b4303753d1594fb2646e5eca955b4bacbb54e
eduard023 <edo.avdzh@yandex.ru>
2022-12-06 20:12:37 +0300

    Лавц кодинг: Напишите программу, которая выводит в консоль все положительные делители натурального числа.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int num = 10;
+        for (int i = 1; i <= num; i++){
+            if (num % i == 0){
+                System.out.println(i);
+            }
+        }
     }
 }
